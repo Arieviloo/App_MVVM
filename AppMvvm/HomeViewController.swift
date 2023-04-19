e2581853cfffc8972288a589bcc452b9957839ea
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-04-19 08:36:48 -0300

    init

---
+++ AppMvvm/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMvvm
+//
+//  Created by Jadiê on 19/04/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
